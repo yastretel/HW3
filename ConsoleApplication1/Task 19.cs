f55b825f4fd5adb5a4ae9262a91404b63e70d2f6
Yaroslav <yas.tretel@gmail.com>
2023-02-24 19:18:59 +0300

    HW Done

---
+++ ConsoleApplication1/Task 19.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+    if (number[0]==number[4] || number[1]==number[3]){
+        Console.WriteLine($"палиндром");
+    }
+    else Console.WriteLine($"НЕ палиндром");
+}
+
+if (number!.Length == 5){
+    CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
